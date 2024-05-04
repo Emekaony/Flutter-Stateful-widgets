d48b91eadf825f5d1af418f9001d5005e3568203
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-05-04 10:30:36 -0700

    Used new widget: SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -25,18 +25,31 @@ class _AppState extends State<App> {
             titleTextStyle: const TextStyle(
                 color: Colors.white, fontWeight: FontWeight.bold),
           ),
-          body: Container(
-            padding: EdgeInsets.all(10),
-            width: double.infinity,
-            height: double.infinity,
-            child: Column(
-              children: [
-                Expanded(child: Text("Count: 1")),
-                FloatingActionButton(
-                  onPressed: () {},
-                  child: Text("Press me!"),
-                )
-              ],
+          body: SafeArea(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              width: double.infinity,
+              height: double.infinity,
+              child: Column(
+                children: [
+                  Expanded(
+                      flex: 10,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Text("Count: 1")],
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      FloatingActionButton(
+                        onPressed: () {},
+                        backgroundColor: Colors.blue,
+                        child: Text("Press me!"),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ));
